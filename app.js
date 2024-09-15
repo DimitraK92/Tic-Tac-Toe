7f57ed31e323dd63665fe9880d7b7588894cb0d7
dkamni <dimitra.kamni@gmail.com>
2024-09-15 21:53:25 +0300

    fixed draw case and made some UI improvements

--- app.js
+++ app.js
@@ -1,70 +1,80 @@
-﻿var move = 1;
-var play = true;
+﻿let move = 1;
+let play = true;
 
 $('table tr td').click(function () {
-    if ($(this).text() == "" && play) {
-        if ((move % 2) == 1) {
+    if ($(this).text() === "" && play) {
+        if ((move % 2) === 1) {
             $(this).append("X");
             $(this).css('color', '#61892f');
         } else {
             $(this).append("O");
             $(this).css('color', '#e85a4f');
         }
-        move++;
-        if (findWinner() != -1 && findWinner() != "") {
-            if (findWinner() == "X") {
-                $('body').append('<div class = "winner"><span>Winner</span>X</div>');
-                $('body').append('<button onclick = "location.reload()">Play Again</button>');
-                $('.winner').css('background-color', '#61892f');
-                $('button').css('color', '#61892f');
+        
+        let winner = findWinner()
+        let winnerDiv =  $('#game .winner')
+        if (winner !== -1 && winner !== "") {
+            winnerDiv.css('visibility', 'visible');
+            winnerDiv.append('<span>Winner</span>');
+            if (winner === "X") {
+                winnerDiv.append('X');
+                winnerDiv.css('background-color', '#61892f');
             } else {
-                $('body').append('<div class = "winner"><span>Winner</span>O</div>');
-                $('body').append('<button onclick = "location.reload()">Play Again</button>');
-                $('.winner').css('background-color', '#e85a4f');
-                $('button').css('color', '#61892f');
+                winnerDiv.append('0');
+                winnerDiv.css('background-color', '#da2b1e');
             }
             play = false;
+        } else if (move === 9) { // last move without winner
+            winnerDiv.css('visibility', 'visible');
+            winnerDiv.append('<span>Draw</span>');
+            winnerDiv.css('background-color', '#666666');
+            play = false;
         }
+        if(!play){
+            $('#game').append('<button onclick = "location.reload()">Play Again</button>');
+            $('button').css('color', '#61892f');
+        }
+        move++;
     }
 })
 
 function findWinner() {
-    sp1 = $('table tr:nth-child(1) td:nth-child(1)').text();
-    sp2 = $('table tr:nth-child(1) td:nth-child(2)').text();
-    sp3 = $('table tr:nth-child(1) td:nth-child(3)').text();
-    sp4 = $('table tr:nth-child(2) td:nth-child(1)').text();
-    sp5 = $('table tr:nth-child(2) td:nth-child(2)').text();
-    sp6 = $('table tr:nth-child(2) td:nth-child(3)').text();
-    sp7 = $('table tr:nth-child(3) td:nth-child(1)').text();
-    sp8 = $('table tr:nth-child(3) td:nth-child(2)').text();
-    sp9 = $('table tr:nth-child(3) td:nth-child(3)').text();
-    //check rows
-    if ((sp1 == sp2) && (sp2 == sp3)) {
+    let sp1 = $('table tr:nth-child(1) td:nth-child(1)').text();
+    let sp2 = $('table tr:nth-child(1) td:nth-child(2)').text();
+    let sp3 = $('table tr:nth-child(1) td:nth-child(3)').text();
+    let sp4 = $('table tr:nth-child(2) td:nth-child(1)').text();
+    let sp5 = $('table tr:nth-child(2) td:nth-child(2)').text();
+    let sp6 = $('table tr:nth-child(2) td:nth-child(3)').text();
+    let sp7 = $('table tr:nth-child(3) td:nth-child(1)').text();
+    let sp8 = $('table tr:nth-child(3) td:nth-child(2)').text();
+    let sp9 = $('table tr:nth-child(3) td:nth-child(3)').text();
+    // check rows
+    if ((sp1 === sp2) && (sp2 === sp3)) {
         return sp3;
     }
-    else if ((sp4 == sp5) && (sp5 == sp6)) {
+    else if ((sp4 === sp5) && (sp5 === sp6)) {
         return sp6;
     }
-    else if ((sp7 == sp8) && (sp8 == sp9)) {
+    else if ((sp7 === sp8) && (sp8 === sp9)) {
         return sp9;
     }
-    //check colums
-    else if ((sp1 == sp4) && (sp4 == sp7)) {
+    // check columns
+    else if ((sp1 === sp4) && (sp4 === sp7)) {
         return sp7;
     }
-    else if ((sp2 == sp5) && (sp5 == sp8)) {
+    else if ((sp2 === sp5) && (sp5 === sp8)) {
         return sp8;
     }
-    else if ((sp3 == sp6) && (sp6 == sp9)) {
+    else if ((sp3 === sp6) && (sp6 === sp9)) {
         return sp9;
     }
-    //check diagonals
-    else if ((sp1 == sp5) && (sp5 == sp9)) {
+    // check diagonals
+    else if ((sp1 === sp5) && (sp5 === sp9)) {
         return sp9;
     }
-    else if ((sp3 == sp5) && (sp5 == sp7)) {
+    else if ((sp3 === sp5) && (sp5 === sp7)) {
         return sp7;
     }
-    //no winner
+    // no winner
     return -1;
 }
